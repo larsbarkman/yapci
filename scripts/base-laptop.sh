95697efd9a47d7bc5b68cac4a35f2f920bc9a865
unknown <lars.barkman@gmail.com>
2022-03-28 10:28:59 +0200

    Made changes to variables are handled

--- scripts/base-laptop.sh
+++ scripts/base-laptop.sh
@@ -11,7 +11,7 @@ then
     exit
 fi
 
-# Set parameters
+# Set variables
 cryptsetup_passphrase = "Set cryptsetup passphrase here"
 hostname = "Set hostname here"
 root_passphrase = "Set root passphrase here"
@@ -33,8 +33,8 @@ mkfs.ext4 /dev/nvme0n1p2
 timedatectl set-ntp true
 
 # Setup encryption on the partition
-echo -n "<${cryptsetup_passphrase}" | cryptsetup luksFormat -q --type luks2 /dev/nvme0n1p2 -
-echo -n "<${cryptsetup_passphrase}" | cryptsetup open /dev/nvme0n1p2 luks -
+echo -n "$cryptsetup_passphrase" | cryptsetup luksFormat -q --type luks2 /dev/nvme0n1p2 -
+echo -n "$cryptsetup_passphrase" | cryptsetup open /dev/nvme0n1p2 luks -
 
 # Create logical volumes 
 pvcreate /dev/mapper/luks
@@ -80,13 +80,13 @@ echo KEYMAP=sv-latin1 >> /etc/vconsole.conf
 echo FONT=ter-128n >> /etc/vconsole.conf
 
 # Create a hostname and hosts file
-echo "${hostname}" > /etc/hostname
+echo "$hostname" >> /etc/hostname
 echo "127.0.0.1	localhost" >> /etc/hosts
 echo "::1 localhost" >> /etc/hosts
 echo "127.0.1.1	${hostname}.localdomain	${hostname}" >> /etc/hosts
 
 # Set a root password
-echo -n "${root_passphrase}" | passwd
+echo -n "$root_passphrase" | passwd
 
 # Configure mkinitcpio with modules needed for the initrd image
 sed -i 's/MODULES=.*/MODULES=(ext4)/' /etc/mkinitcpio.conf
